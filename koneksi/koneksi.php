294cb5b53597ad7bcc5a70c63208c74e54e7a100
dicky16 <setyawandicky88@gmail.com>
2021-04-10 15:59:47 +0800

    ok

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost","root","","katalog_buku");
+$koneksi = mysqli_connect("localhost","dikky","1","katalog_buku");
 // cek koneksi
 if (!$koneksi){
  die("Error koneksi: " . mysqli_connect_errno());
